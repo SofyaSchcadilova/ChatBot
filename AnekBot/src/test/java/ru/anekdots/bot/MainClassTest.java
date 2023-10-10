1c9c4de05e5290794e8bbdb0a911c6d9e606a11b
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-10-10 13:56:23 +0500

    Fixed tests

--- AnekBot/src/test/java/ru/anekdots/bot/MainClassTest.java
+++ AnekBot/src/test/java/ru/anekdots/bot/MainClassTest.java
@@ -1,29 +1,26 @@
 package ru.anekdots.bot;
 
-import org.junit.AfterClass;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.testng.annotations.BeforeClass;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Assert;
 import ru.anekdots.Main;
 import ru.anekdots.resourses.answers;
 
-import static org.junit.jupiter.api.Assertions.*;
-class MainClassTest {
-    @BeforeAll
-    public static void beforeClass() {
+public class MainClassTest {
+    @Before
+    public void beforeClass() {
         System.out.println("Starting testing");
     }
-    @AfterAll
-    public  static void afterClass() {
+    @After
+    public void afterClass() {
         System.out.println("Tests finished");
     }
-    @org.junit.jupiter.api.Test
-    void thinkTest() {
-        assertEquals("Вы написали: Test1 default case", MainClass.think("Test1 default case"));
-        assertEquals(answers._start,MainClass.think("/start"));
-        assertEquals(answers._help,MainClass.think("/help"));
+    @Test
+    public void thinkTest() {
+        Assert.assertEquals("Вы написали: Test1 default case", MainClass.think("Test1 default case"));
+        Assert.assertEquals(answers._start,MainClass.think("/start"));
+        Assert.assertEquals(answers._help,MainClass.think("/help"));
     }
 
 }
