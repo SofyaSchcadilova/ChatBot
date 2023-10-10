9b4d945f8eedc506c1dede1cb406a18567e7dcf7
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-10-10 17:18:41 +0500

    bruh

--- AnekBot/src/main/java/ru.anekdots/resourses/botsdata.java
+++
@@ -1,8 +0,0 @@
-package ru.anekdots.resourses;
-
-// Данные для авторизации бота
-public class botsdata {
-
-    final public static String name = "";
-    final public static String token = "";
-}
---
+++ src/test/java/ru/anekdots/bot/MainClassTest.java
@@ -0,0 +1,28 @@
+package ru.anekdots.bot;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Assert;
+import ru.anekdots.Main;
+import ru.anekdots.resourses.answers;
+
+public class MainClassTest {
+    @Before
+    public void beforeClass() {
+        System.out.println("Starting testing");
+    }
+
+
+    @After
+    public void afterClass() {
+        System.out.println("Tests finished");
+    }
+    @Test
+    public void thinkTest() {
+        Assert.assertEquals("Вы написали: Test1 default case", MainClass.think("Test1 default case"));
+        Assert.assertEquals(answers._start,MainClass.think("/start"));
+        Assert.assertEquals(answers._help,MainClass.think("/help"));
+    }
+
+}
