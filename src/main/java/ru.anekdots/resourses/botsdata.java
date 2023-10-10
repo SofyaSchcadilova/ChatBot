b87cd03d653241ab2fdfe9d054582ed8e8455237
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-10-10 17:25:12 +0500

    Hot Fix

---
+++ src/main/java/ru.anekdots/resourses/botsdata.java
@@ -0,0 +1,8 @@
+package ru.anekdots.resourses;
+
+// Данные для авторизации бота
+public class botsdata {
+
+    final static public String token = "5805156564:AAEbJpeZqwBcWchldQWVYcje8t-D3hqAdiY";
+    final static public String name = "Teledawn_2bot";
+}
