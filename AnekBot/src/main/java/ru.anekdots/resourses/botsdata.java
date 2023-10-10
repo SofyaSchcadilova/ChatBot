738d03c65d7a342e9fec9a65d547137cdb6f4f22
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-10-10 13:09:37 +0500

    Update botsdata.java

--- AnekBot/src/main/java/ru.anekdots/resourses/botsdata.java
+++ AnekBot/src/main/java/ru.anekdots/resourses/botsdata.java
@@ -3,6 +3,6 @@ package ru.anekdots.resourses;
 // Данные для авторизации бота
 public class botsdata {
 
-    final public static String name = "Teledawn_2bot";
-    final public static String token = "5805156564:AAEbJpeZqwBcWchldQWVYcje8t-D3hqAdiY";
+    final public static String name = "";
+    final public static String token = "";
 }
