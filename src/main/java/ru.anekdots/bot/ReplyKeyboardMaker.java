1800e8bc4d058455c06f0aa30333b1e05af2d117
Sofya Schcadilova <SofaSadilova413@gmail.com>
2023-11-06 00:10:59 +0500

    есть кнопки, но они пока не привязаны к отдельным сообщениям

---
+++ src/main/java/ru.anekdots/bot/ReplyKeyboardMaker.java
@@ -0,0 +1,55 @@
+package ru.anekdots.bot;
+
+import com.vdurmont.emoji.EmojiParser;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReplyKeyboardMaker {
+    public ReplyKeyboardMarkup getMainMenuKeyboard(){
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add("нужна помощь");
+        row1.add("нужен анекдот");
+
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add("время анекдота");
+        row2.add("предложить анекдот");
+
+        KeyboardRow row3 = new KeyboardRow();
+        row3.add("все анекдоты");
+        row3.add("лучшие анекдоты");
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        keyboard.add(row1);
+        keyboard.add(row2);
+        keyboard.add(row3);
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        return replyKeyboardMarkup;
+    }
+
+    public ReplyKeyboardMarkup getEvaluationKeyboard(){
+
+        KeyboardRow row = new KeyboardRow();
+        row.add(EmojiParser.parseToUnicode("\uD83D\uDC4D"));
+        row.add(EmojiParser.parseToUnicode("\uD83D\uDC4E"));
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        keyboard.add(row);
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        return replyKeyboardMarkup;
+    }
+}
