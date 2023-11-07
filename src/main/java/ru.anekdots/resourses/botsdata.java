455fe25b590d1d3686ef876b2c6c39b8f66ab535
Polina Kazachenko <114343678+PolinaKazachenko@users.noreply.github.com>
2023-11-07 16:18:33 +0500

    -

--- src/main/java/ru.anekdots/resourses/botsdata.java
+++ src/main/java/ru.anekdots/resourses/botsdata.java
@@ -7,9 +7,9 @@ public class botsdata {
     /**
      * Токен бота
      */
-    final static public String TOKEN = "6608006411:AAHxxxH19u2kfWORCgO5u47vgGoO_Gk2ad0";
+    final static public String TOKEN = "";
     /**
      * Имя бота
      */
-    final static public String NAME = "https://t.me/anekbots_bot";
+    final static public String NAME = "";
 }
