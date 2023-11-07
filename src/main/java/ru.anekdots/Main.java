7c0db25677ede3cc211ab43b58ab3265df41bf73
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-11-07 19:32:45 +0500

    Fixed wrong statement and tests Fixed thread for notification

--- src/main/java/ru.anekdots/Main.java
+++ src/main/java/ru.anekdots/Main.java
@@ -4,6 +4,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.anekdots.bot.TelegramBot;
+import ru.anekdots.bot.ThreadForEverydayJoke;
 import ru.anekdots.databasecontroller.SqlController;
 import ru.anekdots.databasecontroller.models.JokesModel;
 
@@ -11,14 +12,15 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws TelegramApiException, SQLException, ClassNotFoundException, IOException {
+    public static void main(String[] args) throws TelegramApiException, SQLException, ClassNotFoundException, IOException, InterruptedException {
 
 
 
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         TelegramBot tgbot = new TelegramBot();
         telegramBotsApi.registerBot(tgbot);
-        Thread threadForEverydayJoke = new Thread(tgbot.thread);
-        threadForEverydayJoke.start();
+        Thread thread = new Thread(new ThreadForEverydayJoke(tgbot));
+        thread.start();
+
     }
 }
