f0214a8b841e68d2f9ba7e3a38feeebab734b2f8
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-11-19 15:28:07 +0500

    Поиск шуток в интернете
    1) Сделал класс для поиска в google.com
    2) Ещё класс для получения сырой HTML страницы

--- src/main/java/ru.anekdots/logic/HtmlGetter.java
+++ src/main/java/ru.anekdots/logic/HtmlGetter.java
@@ -10,21 +10,14 @@ import java.io.IOException;
 
 public class HtmlGetter {
 
+    public String getHtml(String url) throws IOException {
 
-
-    public void test() throws IOException {
-        String url = "https://www.google.com/search?q=anekdot.ru+%D0%B0%D0%BD%D0%B5%D0%BA%D0%B4%D0%BE%D1%82+%D0%BF%D1%80%D0%BE+%D0%B1%D0%B0%D0%B1%D0%BA%D1%83";
-
-        WebClient webClient = new WebClient(BrowserVersion.CHROME);
-        webClient.getOptions().setJavaScriptEnabled(true); // enable javascript
-        webClient.getOptions().setThrowExceptionOnScriptError(false); //even if there is error in js continue
-        webClient.waitForBackgroundJavaScript(2500); // important! wait until javascript finishes rendering
+        WebClient webClient = new WebClient(BrowserVersion.EDGE);
+        webClient.getOptions().setJavaScriptEnabled(false); // Выключить js
+        webClient.waitForBackgroundJavaScript(1000); // important! wait until javascript finishes rendering
         HtmlPage page = webClient.getPage(url);
 
-        DomNodeList<DomElement> ans = page.getElementsByTagName("a");
-
-        for (DomElement el : ans) {
-            System.out.println(el.getAttribute("href"));
-        }
+        return page.asXml().replaceFirst("<\\?xml version=\"1.0\" encoding=\"(.+)\"\\?>", "<!DOCTYPE html>");
     }
+
 }
