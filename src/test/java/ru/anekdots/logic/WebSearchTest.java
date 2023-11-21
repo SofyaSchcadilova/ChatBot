e452b22bd1eda57c3b1aec5b539a4e391cea1e37
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-11-21 14:45:02 +0500

    Тесты новых классов

--- src/test/java/ru/anekdots/logic/WebSearchTest.java
+++ src/test/java/ru/anekdots/logic/WebSearchTest.java
@@ -1,84 +1,76 @@
 package ru.anekdots.logic;
 
-import org.htmlunit.WebClient;
+import org.htmlunit.html.DomElement;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.TestName;
-import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
-import ru.anekdots.databasecontroller.SqlController;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class WebSearchTest {
 
 
     @Rule
     public TestName testName = new TestName();
-    WebClient webClient;
     String inp;
+
+
     @Before
     public void beforeClass() {
         System.out.println("Starting test " + testName.getMethodName());
-         webClient = Mockito.mock(WebClient.class);
-        MockedConstruction<WebClient> mockedConstruction = Mockito.mockConstruction(WebClient.class);
-        Mockito.doNothing().when(webClient).getOptions().setJavaScriptEnabled(true);
-        Mockito.doNothing().when(webClient).getOptions().setThrowExceptionOnScriptError(false);
-        Mockito.doNothing().when(webClient).waitForBackgroundJavaScript(3000);
     }
-    String answer = "https://www.anekdot.ru/id/-12412\n" +
-            "rt.ru/id/4213\n" +
-            "https://www.bruh.ru/id/-12412";
+    List<String> answer = List.of("https://www.anekdot.ru/id/-12412",
+            "rt.ru/id/4213",
+            "https://www.bruh.ru/id/-12412");
+
     String url = "https://www.google.com/search?q=anekdot.ru+";
     @After
     public void afterClass() { System.out.println("Test finished " + testName.getMethodName());
     }
 
-
-    /*
-        WebClient webClient = new WebClient(BrowserVersion.EDGE);
-        webClient.getOptions().setJavaScriptEnabled(true); // Включить js для корректного поиска
-        webClient.getOptions().setThrowExceptionOnScriptError(false); // Проигнорировать проблемы с js
-        webClient.waitForBackgroundJavaScript(3000); // подождать прогрузки
-        HtmlPage page = webClient.getPage(url+URLEncoder.encode(request,"UTF-8"));
-
-    */
+    /**
+     * Тест на наличие плохих символов
+     * @throws IOException
+     */
     @Test
     void TestFindBannedChars() throws IOException {
 
-        inp = "анекдот про бабку!@#$%";
-        Mockito.when(webClient.getPage(url + URLEncoder.encode(inp,"UTF-8"))).thenReturn(answer);
+        inp = "анекдот про бабку!&*#@$@#$%";
 
+        WebSearch webSearch = Mockito.mock(WebSearch.class);
 
-        WebSearch webSearch = new WebSearch(webClient);
+        Mockito.when(webSearch.getRawLinks(inp)).thenReturn(answer);
+        Mockito.when(webSearch.isContainBannedChars(Mockito.anyString())).thenCallRealMethod();
+        Mockito.when(webSearch.find(Mockito.anyString())).thenCallRealMethod();
 
-        Assert.assertEquals(null,webSearch.find(inp));
+        Assert.assertEquals(null, webSearch.find(inp));
     }
 
+    /**
+     * Основной тест
+     * @throws IOException
+     */
     @Test
     void TestFindFound() throws IOException {
 
-        inp = "анекдот про бабку!";
-        Mockito.when(webClient.getPage(url + URLEncoder.encode(inp,"UTF-8"))).thenReturn(answer);
+        inp = "анекдот про бабку";
 
+        WebSearch webSearch = Mockito.mock(WebSearch.class);
+        Mockito.when(webSearch.getRawLinks(inp)).thenReturn(answer);
+        Mockito.when(webSearch.isContainBannedChars(Mockito.anyString())).thenCallRealMethod();
+        Mockito.when(webSearch.find(Mockito.anyString())).thenCallRealMethod();
 
-        WebSearch webSearch = new WebSearch(webClient);
-        ArrayList<String> out = webSearch.find(inp);
-        ArrayList<String> exp = List.of("https://www.anekdot.ru/id/-12412");
+        ArrayList<String> out = new ArrayList<String>(webSearch.find(inp));
         Assert.assertEquals(1, out.size());
-        String temp1, temp2;
 
 
-        Assert.assertEquals(null,webSearch.find(inp));
+        Assert.assertEquals(answer.get(0),out.get(0));
     }
 }
